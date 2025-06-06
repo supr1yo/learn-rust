bfb7ed5cc887f40b83b9e3b3a9da24199a8b15fe
supr1yo <supriyo.developer@gmail.com>
2025-06-06 18:32:13 +0530

    Add all

--- src/bin/strings.rs
+++ src/bin/strings.rs
@@ -1,13 +1,31 @@
 fn main() {
-    let mut greeting = String::from("Hello World!");
-    greeting.push_str(" Supriyo");
+    /*
+        let mut greeting = String::from("Hello World!");
+        greeting.push_str(" Supriyo");
 
-    println!("{}", greeting);
-    let n = 4;
+        println!("{}", greeting);
+        let n = 4;
 
-    if let Some(c) = greeting.chars().nth(n) {
-        println!("{}", c);
-    } else {
-        println!("No character at position {}", n);
+        if let Some(c) = greeting.chars().nth(n) {
+            println!("{}", c);
+        } else {
+            println!("No character at position {}", n);
+        }
+    */
+    let sentence = String::from("My name is Supriyo");
+    let first_word = get_first_word(sentence);
+    print!("First word is: {}", first_word);
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans = String::new();
+
+    for char in sentence.chars() {
+        if char == ' ' {
+            break;
+        }
+        ans.push(char);
     }
+
+    return ans;
 }
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // variables
+    let name: &str = "Supriyo";
+    let age: i32 = 21;
+    println!("{}{}", name, age);
+}
